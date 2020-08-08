3cf45cc6f028053344143161d6b8c805be7bd739
Alexandrov Vitaliy <aleksandrov.vetall@yandex.ru>
2020-08-08 11:17:24 +0300

    add hashing secret to app config

--- hw2/prj-hw2/src/main/java/ru/spb/avetall/hwarch/service/impl/HashingServiceImpl.java
+++ hw2/prj-hw2/src/main/java/ru/spb/avetall/hwarch/service/impl/HashingServiceImpl.java
@@ -2,6 +2,7 @@ package ru.spb.avetall.hwarch.service.impl;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.spb.avetall.hwarch.service.HashingService;
 
@@ -11,24 +12,31 @@ import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 @Slf4j
 @Service
 public class HashingServiceImpl implements HashingService {
 
-    private final int SALT_LENGTH = 1024;
-    private final int KEY_LENGTH = 1024;
-    private final int ITERATION_COUNT = 65536;
-    private final String ALGORITHM_INSTANCE_NAME = "PBKDF2WithHmacSHA1";
-    private final String SALT = "someSecreteKey";
-    
+    private final String SALT;
+
+    public HashingServiceImpl(@Value("${application.password.salt}") String SALT) {
+        if (isBlank(SALT)) {
+            this.SALT = "someSecretKey";
+        } else {
+            this.SALT = SALT;
+        }
+    }
+
     @Override
     @SneakyThrows({NoSuchAlgorithmException.class, InvalidKeySpecException.class})
     public byte[] generateHash(String password) {
-//        SecureRandom random = new SecureRandom();
-//        byte[] salt = new byte[SALT_LENGTH];
-//        random.nextBytes(salt);
+        int KEY_LENGTH = 1024;
+        int ITERATION_COUNT = 65536;
+
 
         KeySpec spec = new PBEKeySpec(password.toCharArray(), SALT.getBytes(), ITERATION_COUNT, KEY_LENGTH);
+        String ALGORITHM_INSTANCE_NAME = "PBKDF2WithHmacSHA1";
         SecretKeyFactory factory = SecretKeyFactory.getInstance(ALGORITHM_INSTANCE_NAME);
         
         return factory.generateSecret(spec).getEncoded();
